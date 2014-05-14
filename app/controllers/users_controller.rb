7de145d8a87c391680cffa7baf061ff4595e9cbb
davidmccoy <david.mccoy@gmail.com>
2014-05-14 18:09:05 -0400

    Added info to seed json.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,6 +4,8 @@ class UsersController < ApplicationController
     # authorizing a request: (https://dev.twitter.com/docs/auth/authorizing-request)
     oauth_consumer_key = ENV["TWITTER_CONSUMER_KEY"]
     oauth_nonce = SecureRandom.base64(32).gsub(/[^0-9a-z ]/i, '')
+    # needs Post/Get, base url, parameters
+    oauth_signature = 
   end
 
   def application_only_requests
@@ -15,9 +17,18 @@ end
 #response = HTTParty.post('https://api.twitter.com/oauth2/token', :query => { 'grant_type' => 'client_credentials' }, :headers => { 'Authorization' => "Basic #{encoded}", 'Content-Type' => 'application/x-www-form-urlencoded;charset=UTF-8' } )
 
 # basic search
-#result = HTTParty.get("https://api.twitter.com/1.1/search/tweets.json", :query => { 'q' => 'bacon' }, :headers => { 'Authorization' => "Bearer #{ENV['TWITTER_BEARER_TOKEN']}" })
+# response = HTTParty.get("https://api.twitter.com/1.1/search/tweets.json", :query => { 'q' => 'bacon' }, :headers => { 'Authorization' => "Bearer #{ENV['TWITTER_BEARER_TOKEN']}" })
 
-# SIGN IN FLOW
-
-# request token
-# post oauth/request_token
+# find a tweet
+# response = HTTParty.get("https://api.twitter.com/1.1/statuses/show.json", :query => { 'id' => 464468014386409474 }, :headers => { 'Authorization' => "Bearer #{ENV['TWITTER_BEARER_TOKEN']}" })
+# id
+# response["id"]
+# user name
+# response["user"]["name"]
+# profile image
+# response["user"]["profile_image_url"]
+# text
+# response["text"]
+# included media
+# response["entities"]["media"][0]["media_url"]
+# 
