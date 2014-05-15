29e8d07c661c8fb82b643d3627981cbe9339ac3e
davidmccoy <david.mccoy@gmail.com>
2014-05-15 19:17:25 -0400

    fixed menu bar when no one is signed in.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,10 +2,12 @@ class WelcomeController < ApplicationController
 
   def index
 
-    response = HTTParty.get("https://api.twitter.com/1.1/users/show.json", :query => { 'user_id' => current_user.uid }, :headers => { 'Authorization' => "Bearer #{ENV['TWITTER_BEARER_TOKEN']}" })
+    if current_user
+      response = HTTParty.get("https://api.twitter.com/1.1/users/show.json", :query => { 'user_id' => current_user.uid }, :headers => { 'Authorization' => "Bearer #{ENV['TWITTER_BEARER_TOKEN']}" })
 
-    @image = response["profile_image_url"]
-    @screen_name = response["screen_name"]
+      @image = response["profile_image_url"]
+      @screen_name = response["screen_name"]
+    end
   end
 
 end
