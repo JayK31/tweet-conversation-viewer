f8ed2e77740340194f7baad1f77ce8357c2b4599
davidmccoy <david.mccoy@gmail.com>
2014-05-16 10:05:23 -0400

    Current_user fix.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+  
   def create
     @user = User.find_or_create_from_auth_hash(auth_hash)
     self.current_user = @user
@@ -10,4 +11,5 @@ class SessionsController < ApplicationController
   def auth_hash
     request.env['omniauth.auth']
   end
+
 end
